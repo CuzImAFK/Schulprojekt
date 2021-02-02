2b66093499f7630ef7570691d12db0a0a587a3ff
CuzImAFK <67825866+CuzImAFK@users.noreply.github.com>
2021-02-02 17:11:05 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+    system"color 2
+  std::cout << std::string = "Einen Guten Tag wünsche ich, ich freue mich das ich sie für mein projekt begeistern kann. Was kann diese Projekt na ganz einfach es ist einfach eine Katei wo mann information über sich eintragen kann und alte Abrufen kann ich wünsche nun viel spaß Mfg:Moritz";
+
+}
+
+
